f14384e0e2613185df5aa9f44492a1c2e43946ce
Drew DeVault <sir@cmpwn.com>
2012-04-18 16:02:28 -0600

    Updated README and expressions

--- README.md
+++ README.md
@@ -1,22 +1,73 @@
 .orgASM
 =======
 
-.orgASM is an assembler for the DCPU-16 architecture.  It is a work-in-progress.
+.orgASM is an assembler for the DCPU-16 architecture that supports the *.org* directive.  It is an incomplete assembler.  Here's a (likely out of date) list of supported features:
 
-Planned Features
-----------------
-
-* Support for common directives (include, define, **org** etc)
-* Relative addressing
-* Macro support
+* Assembling all instructions
+* Labels
 * Listing output
+* Expression evaluation
 
-Basically, everything you'd expect in a professional-grade assembler.
+*Planned Features*:
 
-The goal of .orgASM is to be able to successfully assemble [this](http://pastebin.com/raw.php?i=W3WTDC09) file.
+* .dw/.dat
+* Relative addressing
+* Backreferences
+* Macros
+
+Using .orgASM
+-------------
+
+On Linux, make sure that you have "mono-complete" installed, and prepend any commands to run .orgASM with "mono", similar to running Java programs.
+
+To compile an assembly file with .orgASM, use the following syntax from the command line:
+
+    orgASM.exe inputfile outputfile
+
+.orgASM will assemble your inputfile and produce a listing in outputfile.  At the present time, binary output is not supported; the assember is the current focus.
+
+Syntax
+------
+
+.orgASM's assembly syntax is the same as notchan assembly.  Here's an example file:
+
+    :label
+        SET A, 10
+        SET B, 0x20
+        ADD A, B
+
+.orgASM is completely case-insensitive.  Additionally, you may pre- or post-fix your label names with ":", whichever you prefer.  .orgASM is also tolerant of any amount of whitespace in any location, though "SE T" is invalid.
+
+### Expressions
+
+Anywhere a number is required, an expression may be used.  Expressions are not evaluated with order-of-operations, but left-to-right, in accordance with assembly standards.  Valid operators are as follows:
+
+    + - / * | & ^ ~ % << >> == != < > <= >= ( )
+
+All values are 16-bit.  Boolean operators return a one or zero as appropriate.
+
+### Pre-processor directives
+
+.orgASM offers several pre-processor directives to ease use.  These may be used with either "." or "#".
+
+*.org [origin]*: Sets the origin to [origin]
+
+*.nolist*: Stops assembly until .list
+
+*.list*: Resumes assembly after .nolist
+
+*.region* and *.endregion*: These are ignored, but do not cause an error.  You may use them to organize a file, and your IDE may support collapsing them.
+
+*.equ [key] (value)* and *.define [key] (value)*: These are identical. The equate a value with a key.  The value is optional - if left out, the default is 1.  You may also use "[key] .equ [value]" for TASM compatibility.
+
+*.ifdef [key]*: If the given key is defined with .equ or .define, this will return true.  The assembler will stop assembing until the next .end or .endif directive if false.
+
+*.endif* and *.end*: Closes a matching .if* statement.
 
 Compiling .orgASM
 -----------------
+
+*Note:* .orgASM's root git directory is ".orgASM" by default, which is hidden on most unix systems.  Use "mv .orgASM orgASM" to fix this.
 
 *Windows*: "msbuild" from the root directory of the project.
 
---
+++ orgASM/Expressions.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+
+namespace orgASM
+{
+    public partial class Assembler
+    {
+        /// <summary>
+        /// Given an expression, it will parse it and return the result as a nullable ushort
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public ushort? ParseExpression(string value)
+        {
+            value = value.Trim();
+            if (value.Contains("("))
+                return EvaluateParenthesis(value);
+            if (value.StartsWith("~"))
+                return (ushort)(~ParseExpression(value.Substring(1)));
+            if (!HasOperators(value))
+            {
+                // Parse value
+                ushort result;
+                if (value.StartsWith("0d"))
+                    value = value.Substring(2);
+                if (value.StartsWith("'")) // Character
+                {
+                    if (value.Length < 3)
+                        return null;
+                    value = value.Substring(1, value.Length - 2).Unescape();
+                    if (value == null)
+                        return null;
+                    if (value.Length != 1)
+                        return null;
+                    return Encoding.ASCII.GetBytes(value)[0];
+                }
+                else if (value.StartsWith("0x")) // Hex
+                {
+                    value = value.Substring(2);
+                    if (!ushort.TryParse(value, NumberStyles.HexNumber, null, out result))
+                        return null;
+                    else
+                    {
+                        return result;
+                    }
+                }
+                else if (value.StartsWith("0b")) // Binary
+                {
+                    value = value.Substring(2);
+                    return ParseBinary(value);
+                }
+                else if (value.StartsWith("0o"))
+                {
+                    value = value.Substring(2);
+                    try
+                    {
+                        return Convert.ToUInt16(value, 8);
+                    }
+                    catch { return null; }
+                }
+                else if (ushort.TryParse(value, out result)) // Decimal
+                {
+                    return result;
+                }
+                else // Defined value or error
+                {
+                    if (Values.ContainsKey(value.ToLower()))
+                    {
+                        return Values[value.ToLower()];
+                    }
+                    else
+                        return null;
+                }
+            }
+
+            // Parse expression
+            string[] operands = GetOperands(value);
+            if (string.IsNullOrEmpty(operands[0]) && operands[1] == "-")
+                return (ushort)-ParseExpression(operands[2]);
+            if (operands == null)
+                return null;
+            ushort? left = ParseExpression(operands[0]);
+            ushort? right = ParseExpression(operands[2]);
+            if (left == null || right == null)
+                return null;
+            switch (operands[1])
+            {
+                case "*":
+                    return (ushort)(left.Value * right.Value);
+                case "/":
+                    return (ushort)(left.Value / right.Value);
+                case "+":
+                    return (ushort)(left.Value + right.Value);
+                case "-":
+                    return (ushort)(left.Value - right.Value);
+                case "<<":
+                    return (ushort)(left.Value << right.Value);
+                case ">>":
+                    return (ushort)(left.Value >> right.Value);
+                case "|":
+                    return (ushort)(left.Value | right.Value);
+                case "^":
+                    return (ushort)(left.Value ^ right.Value);
+                case "&":
+                    return (ushort)(left.Value & right.Value);
+                case "%":
+                    return (ushort)(left.Value % right.Value);
+                case "==":
+                    return (ushort)(left.Value == right.Value ? 1 : 0);
+                case "!=":
+                    return (ushort)(left.Value != right.Value ? 1 : 0);
+                case "<":
+                    return (ushort)(left.Value < right.Value ? 1 : 0);
+                case ">":
+                    return (ushort)(left.Value > right.Value ? 1 : 0);
+                case "<=":
+                    return (ushort)(left.Value <= right.Value ? 1 : 0);
+                case ">=":
+                    return (ushort)(left.Value >= right.Value ? 1 : 0);
+                default:
+                    return null;
+            }
+        }
+
+        private ushort? EvaluateParenthesis(string value)
+        {
+            while (value.Contains("("))
+            {
+                int openingParenthesis = -1, closingParenthesis = -1, parenCount = 0;
+                for (int i = 0; i < value.Length; i++)
+                {
+                    if (value[i] == '(')
+                    {
+                        parenCount++;
+                        if (parenCount == 1)
+                            openingParenthesis = i;
+                    }
+                    if (value[i] == ')')
+                    {
+                        parenCount--;
+                        if (parenCount == 0)
+                        {
+                            closingParenthesis = i;
+                            break;
+                        }
+                    }
+                }
+                if (openingParenthesis == -1 || closingParenthesis == -1)
+                    return null;
+                ushort? subExpression = ParseExpression(value.Substring(openingParenthesis + 1, closingParenthesis - (openingParenthesis + 1)));
+                if (subExpression == null)
+                    return null;
+                value = value.Remove(openingParenthesis) + subExpression.Value.ToString() + value.Substring(closingParenthesis + 1);
+            }
+            return ParseExpression(value);
+        }
+
+        private ushort? ParseBinary(string value)
+        {
+            ushort mask = 1;
+            ushort result = 0;
+            foreach (char c in value)
+            {
+                if (c == '1')
+                    result |= mask;
+                else if (c == '0') ;
+                else
+                    return null;
+                mask <<= 1;
+            }
+            return result;
+        }
+
+        string[] MathOperators = new string[] { "*", "/", "+", "-", "<<", ">>", "|", "^", "&", "%", "==", "!=", ">", "<", ">=", "<=" };
+
+        private string[] GetOperands(string value)
+        {
+            int firstIndex = int.MaxValue;
+            string firstOperator = "";
+            foreach (string s in MathOperators)
+            {
+                if (!value.Contains(s))
+                    continue;
+                bool instring = false, inchar = false;
+                int index = 0;
+                for (int i = 0; i < value.Length; i++)
+                {
+                    if (value[i] == s[index] && !instring && !inchar)
+                    {
+                        if (index == s.Length - 1)
+                        {
+                            // Split string
+                            if (i < firstIndex && firstOperator.Length < s.Length)
+                            {
+                                firstIndex = i;
+                                firstOperator = s;
+                            }
+                            continue;
+                        }
+                        index++;
+                    }
+                    else
+                        index = 0;
+                    if (value[i] == '"')
+                        instring = !instring;
+                    if (value[i] == '\'')
+                        inchar = !inchar;
+                }
+            }
+            if (firstIndex != int.MaxValue)
+            {
+                return new string[]
+                {
+                    value.Remove(firstIndex - (firstOperator.Length - 1)),
+                    firstOperator,
+                    value.Substring(firstIndex + 1)
+                };
+            }
+            return null;
+        }
+
+        private bool HasOperators(string value)
+        {
+            foreach (string s in MathOperators)
+            {
+                if (!value.Contains(s))
+                    continue;
+                bool instring = false, inchar = false;
+                int index = 0;
+                foreach (char c in value)
+                {
+                    if (c == s[index] && !instring && !inchar)
+                    {
+                        if (index == s.Length - 1)
+                            return true;
+                        index++;
+                    }
+                    else
+                        index = 0;
+                    if (c == '"')
+                        instring = !instring;
+                    if (c == '\'')
+                        inchar = !inchar;
+                }
+            }
+            return false;
+        }
+    }
+}
