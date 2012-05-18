354cef1a040e957f707d4a716b20026537352637
Drew DeVault <sir@cmpwn.com>
2012-05-18 09:16:46 -0600

    Adding plugin docs

---
+++ Organic/Plugins/HandleCodeEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Organic.Plugins
+{
+    public class HandleCodeEventArgs
+    {
+        public string Code { get; set; }
+        public bool Handled { get; set; }
+        public ListEntry Output { get; set; }
+    }
+}
